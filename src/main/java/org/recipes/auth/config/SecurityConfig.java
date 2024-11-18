a84fff7e4591be37856e4593c5ba9b72d9ee46db
Joe Maskery <joe.maskery@yahoo.co.uk>
2024-11-18 20:27:12 +0000

    fix CORS security

--- src/main/java/org/recipes/auth/config/SecurityConfig.java
+++ src/main/java/org/recipes/auth/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -35,7 +36,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http, AuthenticationManager authenticationManager) throws Exception {
         return http
-                .cors(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 // Set permissions on endpoints
--- src/main/java/org/recipes/auth/controller/AuthController.java
+++ src/main/java/org/recipes/auth/controller/AuthController.java
@@ -9,15 +9,13 @@ import org.recipes.auth.dto.LoginResponse;
 import org.recipes.user.dto.User;
 import org.recipes.user.service.UserService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
 @AllArgsConstructor
 @Slf4j
+@CrossOrigin
 public class AuthController {
 
     private final AuthService authService;
